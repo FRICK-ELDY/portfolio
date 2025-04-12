17675e6b39579debe906c530ceda5649457b92da
FRICK-ELDY <ncssano0x0000@gmail.com>
2025-04-13 01:56:18 +0900

    add view

---
+++ umbrella/apps/top/lib/top_web/live_view/top_live_view.ex
@@ -0,0 +1,13 @@
+defmodule TopWeb.TopLiveView do
+  use TopWeb, :view
+
+  @gettext_domain "top"
+
+  def t(text) do
+    Gettext.dgettext(Shared.Gettext, @gettext_domain, text)
+  end
+
+  def t(text, bindings) when is_list(bindings) do
+    Gettext.dgettext(Shared.Gettext, @gettext_domain, text, bindings)
+  end
+end
